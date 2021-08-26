951b863883eb096f382647dbe549b83fc73051c4
glennk4 <79699818+glennk4@users.noreply.github.com>
2021-08-26 10:49:07 +0100

    Add project files.

---
+++ CD Account Test/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿
+namespace CD_Account_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.AccountInput = new System.Windows.Forms.TextBox();
+            this.InterestInput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BalanceInput = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.MaturityInput = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.AccountOutput = new System.Windows.Forms.Label();
+            this.InterestOutput = new System.Windows.Forms.Label();
+            this.BalanceOutput = new System.Windows.Forms.Label();
+            this.MaturityOutput = new System.Windows.Forms.Label();
+            this.CreateButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.MaturityInput);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.BalanceInput);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.InterestInput);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.AccountInput);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(411, 58);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(281, 219);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Enter CD Account Data";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Account Number:";
+            // 
+            // AccountInput
+            // 
+            this.AccountInput.Location = new System.Drawing.Point(152, 41);
+            this.AccountInput.Name = "AccountInput";
+            this.AccountInput.Size = new System.Drawing.Size(100, 22);
+            this.AccountInput.TabIndex = 1;
+            // 
+            // InterestInput
+            // 
+            this.InterestInput.Location = new System.Drawing.Point(152, 82);
+            this.InterestInput.Name = "InterestInput";
+            this.InterestInput.Size = new System.Drawing.Size(100, 22);
+            this.InterestInput.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Interest Rate: ";
+            // 
+            // BalanceInput
+            // 
+            this.BalanceInput.Location = new System.Drawing.Point(152, 122);
+            this.BalanceInput.Name = "BalanceInput";
+            this.BalanceInput.Size = new System.Drawing.Size(100, 22);
+            this.BalanceInput.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(82, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Balance: ";
+            // 
+            // MaturityInput
+            // 
+            this.MaturityInput.Location = new System.Drawing.Point(152, 162);
+            this.MaturityInput.Name = "MaturityInput";
+            this.MaturityInput.Size = new System.Drawing.Size(100, 22);
+            this.MaturityInput.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(57, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Maturity Date:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.MaturityOutput);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.BalanceOutput);
+            this.groupBox2.Controls.Add(this.AccountOutput);
+            this.groupBox2.Controls.Add(this.InterestOutput);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Location = new System.Drawing.Point(411, 294);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(281, 219);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Enter CD Account Data";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(57, 165);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 16);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Maturity Date:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(82, 125);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 16);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Balance: ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(57, 85);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(89, 16);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Interest Rate: ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(36, 44);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(110, 16);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Account Number:";
+            // 
+            // AccountOutput
+            // 
+            this.AccountOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.AccountOutput.Location = new System.Drawing.Point(152, 38);
+            this.AccountOutput.Name = "AccountOutput";
+            this.AccountOutput.Size = new System.Drawing.Size(100, 22);
+            this.AccountOutput.TabIndex = 8;
+            // 
+            // InterestOutput
+            // 
+            this.InterestOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.InterestOutput.Location = new System.Drawing.Point(152, 84);
+            this.InterestOutput.Name = "InterestOutput";
+            this.InterestOutput.Size = new System.Drawing.Size(100, 22);
+            this.InterestOutput.TabIndex = 9;
+            // 
+            // BalanceOutput
+            // 
+            this.BalanceOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.BalanceOutput.Location = new System.Drawing.Point(152, 124);
+            this.BalanceOutput.Name = "BalanceOutput";
+            this.BalanceOutput.Size = new System.Drawing.Size(100, 22);
+            this.BalanceOutput.TabIndex = 10;
+            // 
+            // MaturityOutput
+            // 
+            this.MaturityOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.MaturityOutput.Location = new System.Drawing.Point(152, 164);
+            this.MaturityOutput.Name = "MaturityOutput";
+            this.MaturityOutput.Size = new System.Drawing.Size(100, 22);
+            this.MaturityOutput.TabIndex = 11;
+            // 
+            // CreateButton
+            // 
+            this.CreateButton.Location = new System.Drawing.Point(411, 531);
+            this.CreateButton.Name = "CreateButton";
+            this.CreateButton.Size = new System.Drawing.Size(123, 48);
+            this.CreateButton.TabIndex = 9;
+            this.CreateButton.Text = "Create Object";
+            this.CreateButton.UseVisualStyleBackColor = true;
+            this.CreateButton.Click += new System.EventHandler(this.CreateButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(563, 531);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(123, 48);
+            this.ExitButton.TabIndex = 10;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.CreateButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1147, 615);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.CreateButton);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "CD Account Test";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox MaturityInput;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox BalanceInput;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox InterestInput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox AccountInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label MaturityOutput;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label BalanceOutput;
+        private System.Windows.Forms.Label AccountOutput;
+        private System.Windows.Forms.Label InterestOutput;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button CreateButton;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
+
---
+++ CD Account Test/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CD_Account_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close(); 
+        }
+    }
+}
